b1475dfefe03bec2da4a037add18cc25bc27a18a
alejandro197mejia <alejandro197mejia@gmail.com>
2025-09-27 01:43:35 +0000

    feat: Agregar archivo de configuración de Firebase

---
+++ firebase-auth-project/src/app/config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAh32ES7WuNvWOlaFOmb0FQ1ICySN1AYUQ",
+  authDomain: "mediainfo-19346.firebaseapp.com",
+  projectId: "mediainfo-19346",
+  storageBucket: "mediainfo-19346.firebasestorage.app",
+  messagingSenderId: "261145934389",
+  appId: "1:261145934389:web:3f82abc01e3f8cc6718f7c"
+};
