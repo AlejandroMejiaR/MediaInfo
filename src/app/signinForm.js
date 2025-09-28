527c0c873be862e8e17017401a370fd478e6064b
alejandro197mejia <alejandro197mejia@gmail.com>
2025-09-28 22:50:55 +0000

    Feat: Añadida funcionalidad de restablecer contraseña

--- src/app/signinForm.js
+++ src/app/signinForm.js
@@ -1,12 +1,13 @@
 
 import './main.js'
-import { GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
+import { GoogleAuthProvider, signInWithPopup, sendPasswordResetEmail } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
 import { signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-auth.js"
 import { auth } from './firebase.js'
 import { showMessage } from './showMessage.js'
 
 const googleButton = document.querySelector('#google-login')
 const signInForm = document.querySelector('#signin-form')
+const forgotPassword = document.querySelector('#forgot-password')
 
 signInForm.addEventListener('submit', async (e) => {
     e.preventDefault()
@@ -39,3 +40,23 @@ googleButton.addEventListener('click', async () => {
         console.log(error)
     }
 })
+
+forgotPassword.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const email = signInForm['signin-email'].value;
+    if (email === "") {
+        showMessage("Por favor, ingrese su correo electrónico para restablecer la contraseña.", "error");
+        return;
+    }
+    try {
+        await sendPasswordResetEmail(auth, email);
+        showMessage("Se ha enviado un correo electrónico para restablecer la contraseña.", "success");
+    } catch (error) {
+        if (error.code === 'auth/user-not-found') {
+            showMessage("El usuario no esta registrado. ", "error");
+        } else {
+            showMessage("Algo salió mal", "error");
+            console.log(error)
+        }
+    }
+});
